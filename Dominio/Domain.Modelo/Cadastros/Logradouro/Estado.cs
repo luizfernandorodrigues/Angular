c5566792b8a0720beb635f55e6da5e59833de314
luizfernandorodrigues <luiz@jmti.com.br>
2019-05-08 20:37:57 -0300

    merge

---
+++ Dominio/Domain.Modelo/Cadastros/Logradouro/Estado.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Dominio.Modelo
+{
+    public class Estado : DominioAbstract
+    {
+        public string Nome { get; set; }
+        public string Sigla { get; set; }
+        public Guid Pais_Ukey { get; set; }
+        public ICollection<Cidade> Cidades { get; set; }
+    }
+}
--- Dominio/Domain.Modelo/Cadastros/Logradouro/Pais.cs
+++ Dominio/Domain.Modelo/Cadastros/Logradouro/Pais.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Domain.Dominio.Modelo
+﻿namespace Domain.Dominio.Modelo
 {
     public class Pais : DominioAbstract
     {
         public string Nome { get; set; }
         public string Codigo { get; set; }
         public string Observacoes { get; set; }
+        public ICollection<Estado> Estados { get; set; }
     }
 }
