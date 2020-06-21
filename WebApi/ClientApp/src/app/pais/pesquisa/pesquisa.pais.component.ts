5ef6f2751eb0c76d1ee4add06b40dca658867f60
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-06-21 13:20:28 -0300

    Inicio da implementação do template de pesquisa

--- WebApi/ClientApp/src/app/pais/pesquisa/pesquisa.pais.component.ts
+++ WebApi/ClientApp/src/app/pais/pesquisa/pesquisa.pais.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Pais } from '../../modelo/cadastro/logradouro/pais';
-import { PaisServico } from '../../servicos/cadastro/logradouro/pais.servico';
+import { Component, OnInit } from "@angular/core";
+import { Pais } from "../../modelo/cadastro/logradouro/pais";
+import { PaisServico } from "../../servicos/cadastro/logradouro/pais.servico";
 
 @Component({
-  selector: 'pesquisa-produto',
-  templateUrl: './pesquisa.pais.component.html',
-  styleUrls: ['./pesquisa.pais.component.html']
+  selector: "pesquisa-produto",
+  templateUrl: "./pesquisa.pais.component.html",
+  styleUrls: ["./pesquisa.pais.component.html"],
 })
 export class PesquisaPaisComponent implements OnInit {
-
   public paises: Pais[];
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   constructor(private paisServico: PaisServico) {
-    this.paisServico.obterPaises().subscribe(data => {
-      this.paises = data;
-    }, erro => {
-        console.log('Ocorreu um erro: ' + erro.error);
-    })
+    this.paisServico.obterPaises().subscribe(
+      (data) => {
+        this.paises = data;
+      },
+      (erro) => {
+        console.log("Ocorreu um erro: " + erro.error);
+      }
+    );
   }
 }
