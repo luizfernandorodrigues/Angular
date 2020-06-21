6c01d28a66dd70914000106d99265d9cd2de07dd
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-06-21 13:23:18 -0300

    Comit e ajustes

--- WebApi/Controllers/AController.cs
+++ WebApi/Controllers/AController.cs
@@ -3,28 +3,28 @@ using System.Threading.Tasks;
 
 namespace WebApi.Controllers
 {
-    public abstract class AController : ControllerBase
+  public abstract class AController : ControllerBase
+  {
+    protected bool OperacaoValida { get; set; }
+    public AController()
     {
-        protected bool OperacaoValida { get; set; }
-        public AController()
-        {
 
-        }
-
-        protected ActionResult CustomResponse(object objeto = null)
-        {
-            if (OperacaoValida)
-                return Ok(new
-                {
-                    success = true,
-                    data = objeto
-                });
-
-            return BadRequest(new
-            {
-                success =false,
-                data = objeto
-            });
-        }
     }
+
+    protected ActionResult CustomResponse(object objeto = null)
+    {
+      if (OperacaoValida)
+        return Ok(new
+        {
+          success = true,
+          data = objeto
+        });
+
+      return BadRequest(new
+      {
+        success = false,
+        data = objeto
+      });
+    }
+  }
 }
